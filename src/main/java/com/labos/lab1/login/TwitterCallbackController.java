8b4680ebf7c30c7cff007224e52e3b407337665b
Josip Frkin <josip.frk@gmail.com>
2022-01-08 18:01:44 +0100

    Added twitter login

--- src/main/java/com/labos/lab1/login/TwitterCallbackController.java
+++ src/main/java/com/labos/lab1/login/TwitterCallbackController.java
@@ -1,35 +1,36 @@
 package com.labos.lab1.login;
 
-import javax.servlet.http.Cookie;
+import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.labos.lab1.user.User;
-import com.labos.lab1.user.UserRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import twitter4j.Twitter;
+import twitter4j.User;
 import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
 
 @Controller
 public class TwitterCallbackController {
 
-    @Autowired
-    private UserRepository userRepository;
-    
+    private static final Logger LOGGER = LoggerFactory.getLogger(TwitterCallbackController.class);
+
     @RequestMapping("/twitterCallback")
     public String twitterCallback(@RequestParam(value="oauth_verifier", required=false) String oauthVerifier,
-    @RequestParam(value="denied", required=false) String denied,
-    HttpServletRequest request, HttpServletResponse response, Model model){
+                                  @RequestParam(value="denied", required=false) String denied,
+                                  HttpServletRequest request, HttpServletResponse response, Model model) {
+
         if (denied != null) {
-            return "redirect:index";
+            return "redirect:login";
         }
 
         Twitter twitter = (Twitter) request.getSession().getAttribute("twitter");
@@ -37,25 +38,14 @@ public class TwitterCallbackController {
 
         try {
             AccessToken token = twitter.getOAuthAccessToken(requestToken, oauthVerifier);
-            System.out.println("before if");
-            if(userRepository.findByTwitterId(twitter.getId()).isEmpty()){
-                userRepository.save(new User(twitter.getScreenName(), twitter.getId(), twitter.showUser(twitter.getId()).get400x400ProfileImageURL()));
-                System.out.println("spremijo u bazu");
-            }
-            System.out.println("after if");
-            HttpHeaders header = new HttpHeaders();
-            header.add(HttpHeaders.AUTHORIZATION, "Bearer " + token.getToken());
-            System.out.println(twitter.getId() + " " + twitter.getScreenName());
-            request.getSession().setAttribute("screenname", twitter.getScreenName());
-            response.addCookie(new Cookie("username", twitter.getScreenName()));
-            response.addCookie(new Cookie("userId", Long.toString(twitter.getId())));
-
-            model.addAttribute("username", twitter.getScreenName());
-            
-            return "index";
-        } catch(Exception e){
-            System.out.println(e.getMessage());
-            return "redirect:";
+            SecurityContext context = SecurityContextHolder.createEmptyContext();
+            Authentication auth = new UsernamePasswordAuthenticationToken(twitter.getScreenName(), null, new ArrayList<>());
+            context.setAuthentication(auth);
+            SecurityContextHolder.setContext(context);
+            return "redirect:/";
+        } catch (Exception e) {
+            LOGGER.error("Problem getting token!",e);
+            return "redirect:login";
         }
     }
 }
