234207780ed8b000f7700d89b6d8e216be85b6b9
Josip Frkin <josip.frk@gmail.com>
2022-01-09 21:52:26 +0100

    Fixed index page

--- src/main/java/com/labos/lab1/movie/MovieService.java
+++ src/main/java/com/labos/lab1/movie/MovieService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.gson.Gson;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -43,6 +44,10 @@ public class MovieService {
 
   public List<Movie> findBestMovies() {
     return movieRepository.findBestMovies();
+  }
+
+  public List<Movie> findAllMovies() {
+    return movieRepository.findAll(Sort.by(Sort.Direction.ASC, "title")).subList(0, 300);
   }
 
   public List<Movie> findFilteredMovies(MovieFilter filter) {
