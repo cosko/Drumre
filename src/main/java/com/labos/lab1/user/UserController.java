d6b01e6570e52d5d5e7bc362aa447c4c231c06ed
Josip Frkin <josip.frk@gmail.com>
2022-01-09 23:07:19 +0100

    Dodan prikaz lajkanih stranica za facebook korisnika

--- src/main/java/com/labos/lab1/user/UserController.java
+++ src/main/java/com/labos/lab1/user/UserController.java
@@ -1,15 +1,8 @@
 package com.labos.lab1.user;
 
-import java.security.Principal;
-import java.util.Map;
-
 import com.labos.lab1.integration.FacebookService;
 import com.labos.lab1.integration.LikedPageList;
-import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,9 +23,10 @@ public class UserController {
 
   @GetMapping
   public String getUser(Model model, Authentication auth){
-    // LikedPageList userLikedPages = fbService.getUserLikedPages();
+    LikedPageList userLikedPages = fbService.getUserLikedPages();
 
     model.addAttribute("user", userService.getUserFromAuth(auth));
+    model.addAttribute("likedPages", userLikedPages);
     return "pages/profile";
   }
 }
